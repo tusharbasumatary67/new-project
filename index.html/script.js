8f67b99be8a482158d82a75265ba96c6ea953c20
tusharbasumatary67 <tusharbasumatary67@gmail.com>
2024-12-04 21:24:20 +0530

    added index.html

---
+++ index.html/script.js
@@ -0,0 +1,7 @@
+
+// Simple button interaction
+document.getElementById("clickButton").addEventListener("click", function () {
+    const message = document.getElementById("message");
+    message.textContent = "SANJANA kuti sala gundi reply nahi karta hai bll ka ";
+    message.style.color = "black";
+});
